e50ad283f0f5e13c1c7a2109d7f8794541dded91
Mahaprabu <mahaprabucareer9@gmail.com>
2025-08-27 10:08:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+const reactDescriptions = ['Fundamental','Crucial','Core'];
+
+function getRandomInt(max){
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  return (
+    <div> 
+      <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {reactDescriptions[getRandomInt(2)]} Fundamental React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+function App(){
+  return (
+<div>
+  <Header/>
+
+   <main>
+        <h2>Time to get started!</h2>
+      </main>
+</div>
+  );
+}
+export default App;
