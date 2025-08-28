fe0cfb1171d0e44e57f125c51cef44fa1fa69ad1
Mahaprabu <mahaprabucareer9@gmail.com>
2025-08-28 16:29:08 +0530

    Components split up

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,11 @@
-import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png'
 import {CORE_CONCEPTS} from './data.js'
-
-const reactDescriptions = ['Fundamental','Crucial','Core'];
-
-function getRandomInt(max){
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  return (
-    <div> 
-      <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {reactDescriptions[getRandomInt(2)]} Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-    </div>
-  );
-}
-
-function CoreConcept(props){
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  )
-}
+import Header from './components/Header.jsx'
+import CoreConcept from './components/CoreConcepts.jsx';
 
 function App(){
   return (
 <div>
   <Header/>
-
    <main>
     <section id="core-concepts">
      <h2>Core Concepts</h2> 
@@ -47,21 +15,9 @@ function App(){
         description={CORE_CONCEPTS[0].description}
         image={CORE_CONCEPTS[0].image}
         />
-      <CoreConcept 
-      title={CORE_CONCEPTS[1].title}
-      description={CORE_CONCEPTS[1].description}
-      image={CORE_CONCEPTS[1].image}
-      />
-      <CoreConcept
-      title={CORE_CONCEPTS[2].title}
-      description={CORE_CONCEPTS[2].description}
-      image={CORE_CONCEPTS[2].image}
-      />
-      <CoreConcept
-      title={CORE_CONCEPTS[3].title}
-      description={CORE_CONCEPTS[3].description}
-      image={CORE_CONCEPTS[3].image}
-      />
+      <CoreConcept {...CORE_CONCEPTS[1]}/>
+      <CoreConcept {...CORE_CONCEPTS[2]}/>
+      <CoreConcept {...CORE_CONCEPTS[3]}/>
      </ul>
     </section>
         <h2>Time to get started!</h2>
